f0ac8ed2b8ebfb1698a3122ca8a8da58d162d77f
Jack Sheridan <jack.sheridan@tieto.com>
2019-04-09 09:41:04 +0300

    Add H2 in mem DB config for test

---
+++ src/test/java/com/sap/bulletinboard/ads/config/EmbeddedDatabaseConfig.java
@@ -0,0 +1,52 @@
+package com.sap.bulletinboard.ads.config;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import com.sap.bulletinboard.ads.models.Advertisement;
+import com.sap.bulletinboard.ads.models.AdvertisementRepository;
+import com.sap.bulletinboard.ads.util.EntityManagerFactoryProvider;
+
+/**
+ * See CloudDatabaseConfig for more detailed comments.
+ *
+ * Provides a convenient repository, based on JPA (EntityManager, TransactionManager).
+ */
+@Configuration
+@EnableJpaRepositories(basePackageClasses = AdvertisementRepository.class)
+public class EmbeddedDatabaseConfig {
+
+
+    /*
+    Create data source
+     */
+    @Bean
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+    }
+
+    /*
+    Based on data source, provides entity management
+     */
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource){
+        return EntityManagerFactoryProvider.get(dataSource, Advertisement.class.getPackage().toString());
+    }
+
+    /*
+    Provides TransactManager (JPA) based on entity config
+     */
+    @Bean(name = "transactionManager")
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
+}
